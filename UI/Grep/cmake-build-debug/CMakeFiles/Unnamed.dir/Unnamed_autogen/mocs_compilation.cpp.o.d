a234440463a5e478013436d81a9f68088848378c
Amirhosseinfsh <amirhosseinfsh2000@gmail.com>
2024-02-04 19:01:30 +0330

    fake Commit

---
+++ UI/Grep/cmake-build-debug/CMakeFiles/Unnamed.dir/Unnamed_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/Unnamed.dir/Unnamed_autogen/mocs_compilation.cpp.o: \
+ /home/amirhossein/Desktop/osProject/UI/Grep/cmake-build-debug/Unnamed_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
